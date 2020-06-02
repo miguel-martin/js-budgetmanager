0c092e5366c0fbcf2ef62e5da4e9cc09d216d22f
Miguel Martín <miguel@MacBook-Pro-de-Miguel.local>
2020-06-02 18:42:24 +0200

    Setting up a event listener to pressed keys

--- app.js
+++ app.js
@@ -22,8 +22,25 @@ var UIController = (function(){
 var controller = (function(budgetCtrl, UICtrl){
 
     document.querySelector('.add__btn').addEventListener('click', function() {
-        console.log("Button was clicked");
+        
+        // 1. Get the filed input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the new item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+    });
+
+
+    document.addEventListener('keypress', function(event){
+        console.log(event); // if you check the keyCode property you will see the key that was pressed!
     });
     
 
 })(budgetController, UIController);
+
+
